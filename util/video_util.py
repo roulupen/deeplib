eb4d005556d42042b23bc478dd2a0f89eb2bf588
Upendra Roul <roul@adobe.com>
2020-02-23 12:40:52 +0530

    Refactored code.

--- util/video_util.py
+++ util/video_util.py
@@ -15,39 +15,45 @@ def make_video(outvid, images=None, fps=30, size=None, is_color=True, format="FM
     It will resize every image to this size before adding them to the video.
     """
     from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
-    fourcc = VideoWriter_fourcc (*format)
+    import os
+
+    fourcc = VideoWriter_fourcc(*format)
     vid = None
     for image in images:
-        if not os.path.exists (image):
-            raise FileNotFoundError (image)
-        img = imread (image)
+        if not os.path.exists(image):
+            raise FileNotFoundError(image)
+        img = imread(image)
         if vid is None:
             if size is None:
                 size = img.shape[1], img.shape[0]
-            vid = VideoWriter (outvid, fourcc, float (fps), size, is_color)
+            vid = VideoWriter(outvid, fourcc, float(fps), size, is_color)
         if size[0] != img.shape[1] and size[1] != img.shape[0]:
-            img = resize (img, size)
-        vid.write (img)
-    vid.release ()
+            img = resize(img, size)
+        vid.write(img)
+    vid.release()
     return vid
 
 
-def make_video_from_image_directory(images_dir, output_video_dir, video_file_name):
+def make_video_from_image_directory(images_dir, output_video_dir, video_file_name, fps=30):
     """
         Create a video from a list of images.
 
-        @param      images_dir          Input images directory
-        @param      output_video_dir    Output video directory
-        @param      video_file_name     File Name.
+        @:param     images_dir          Input images directory
+        @:param     output_video_dir    Output video directory
+        @:param     video_file_name     File Name.
+        @:param     fps                 Frames per sec
+        @:return    output_video_path   Out out video path
     """
     import glob
     import os
 
     # Directory of images to run detection on
-    images = list (glob.iglob (os.path.join (images_dir, '*.jpg')))
+    images = list(glob.iglob (os.path.join (images_dir, '*.jpg')))
 
     # Sort the images by integer index
-    images = sorted (images, key=lambda x: float (os.path.split (x)[1].split ("_")[0]))
+    images = sorted(images, key=lambda x: float (os.path.split (x)[1].split ("_")[0]))
 
-    outvid = os.path.join (output_video_dir, video_file_name)
-    make_video(outvid, images, fps=30)
+    output_video_path = os.path.join(output_video_dir, video_file_name)
+    make_video(output_video_path, images, fps=30)
+
+    return output_video_path
